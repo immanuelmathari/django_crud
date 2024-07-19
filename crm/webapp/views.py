319ba1a456125d1290e59ed56aedf99ede443579
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-19 08:44:52 +0300

    first commit

---
+++ crm/webapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
+
+# Create your views here.
+
+def home(request):
+    # return HttpResponse("Hello world")
+    return render(request, 'webapp/index.html')
+
+
+# Register a user
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            # return redirect('')
+    
+    context = {'form' : form}
+    return render(request, 'webapp/register.html', context=context)
+    
