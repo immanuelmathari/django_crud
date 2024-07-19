c692c439c8a3ef4e64bb339d3eb1094cbd38cac4
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-19 10:04:05 +0300

    login functionality

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name=""),
     
     path('register', views.register, name="register"),
+    
+    path('my-login', views.my_login, name="my-login"),
 ]
--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CreateUserForm, LoginForm
+# to logout and authenticate
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
@@ -19,6 +22,25 @@ def register(request):
             form.save()
             # return redirect('')
     
-    context = {'form' : form} # this helps us to render username,password from createUserForm to our template register.html
+    context = {'form' : form} # this helps us to render username,password from createUserForm to our template register.html. its called a context dictionary in django
     return render(request, 'webapp/register.html', context=context)
+
+# login a user
+def my_login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            # the first one is for the database
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                auth.login(request,user)
+                #return redirect('')
     
+    context = {'form2': form} #take data to login.html
+    return render(request, 'webapp/my-login.html', context=context)
+        
