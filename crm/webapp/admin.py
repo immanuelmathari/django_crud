7a06f0733a9e1455c4bcec9946477c0f889c11ff
Evil Genius <immanuelnganga2001@gmail.com>
2024-07-20 10:46:38 +0300

    creating a model

--- crm/webapp/admin.py
+++ crm/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . models import Record # we are in the same directory
+admin.site.register(Record)
+
+
+
--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -4,6 +4,9 @@ from .forms import CreateUserForm, LoginForm
 # to logout and authenticate
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
+# to make sure only authenticated users access restricted pages
+from django.contrib.auth.decorators import login_required
+from . models import Record
 
 # Create your views here.
 
@@ -20,7 +23,7 @@ def register(request):
         
         if form.is_valid():
             form.save()
-            # return redirect('')
+            return redirect('my-login')
     
     context = {'form' : form} # this helps us to render username,password from createUserForm to our template register.html. its called a context dictionary in django
     return render(request, 'webapp/register.html', context=context)
@@ -39,8 +42,20 @@ def my_login(request):
             
             if user is not None:
                 auth.login(request,user)
-                #return redirect('')
+                return redirect('dashboard')
     
     context = {'form2': form} #take data to login.html
     return render(request, 'webapp/my-login.html', context=context)
-        
+
+# - Dashboard view
+@login_required(login_url='my-login') # ensures that only authorized users access dashboard
+def dashboard(request):
+    my_records = Record.objects.all()
+    context = {'records' : my_records}
+    return render(request, 'webapp/dashboard.html', context=context)
+
+# - User Logout
+def user_logout(request):
+    auth.logout(request)
+    
+    return redirect("my-login")
