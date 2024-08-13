fcb6228fa8fd708dce3b8c56d37cead9bbaebd82
Evil Genius <immanuelnganga2001@gmail.com>
2024-08-13 12:40:34 +0300

    changes of review

--- crm/static/js/app.js
+++ crm/static/js/app.js
@@ -1,3 +1,4 @@
+// this comes from the id message timer in base.html for the message popup timer. the alert
 var message_timeout = document.getElementById("message-timer");
 
 setTimeout(function() 
